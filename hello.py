174ee0ed1290e38fef0f2f1306f2f58e7905f3ff
Eric Xie <eric.xie@duke.edu>
2021-06-03 00:26:38 -0400

    initial commit of python day 2

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("Hello World!")
+
+## There's something called String interpolation:
+
+name = "Eric"
+drink = "Sprite"
+
+print("{}, would you like a {}?" .format(name, drink))
+
+## This is the most update to date use of it (fstring):
+
+print(f"{name}, would you like a {drink}?")
