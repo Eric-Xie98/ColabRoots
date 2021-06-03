8f173c6e509b287ee215d93f8c30f71aabff5aeb
Eric Xie <eric.xie@duke.edu>
2021-06-03 12:46:01 -0400

    completed USER_INPUT.md bonus from class

--- user_input.py
+++ user_input.py
@@ -2,32 +2,64 @@
 
 nums = []
 
+isString = False
+isError = False
+
+first = 0
+
 while True:
 
-    user_num = input("Please enter a number: ")
+    user_num = input("Please enter a number or a string: ")
     if(user_num == ""):
         break
 
-    try:
-        val = float(user_num)
-    except ValueError:
-        print("That's not a number!")
-        continue
+    if(isString):
+        try:
+            val = float(user_num)
+            print("You tried to input a number into a string list")
+            isError = True
+            break
+        except ValueError:
+            nums.append(user_num)
+    else:
+        try:
+            val = float(user_num)
+            nums.append(val)
+        except ValueError:
+            if(first == 0):
+                isString = True
+                nums.append(user_num)
+            else:
+                print("You tried to enter a string into a number list")
+                isError = True
+                break
+    
+    first += 1
 
-    nums.append(user_num)
+
 
 total = 0
 count = len(nums)
 
-for num in nums:
-    total += float(num)
+if isError:
+    pass
 
-try:
-    average = total / count
-except ZeroDivisionError:
-    print("You can't divide a number by zero!")
+elif not isString:
+    for num in nums:
+        total += float(num)
 
-print(f"The total amount inputted by the user is {total}" )
-print(f"The average number is {average}")
+    try:
+        average = total / count
+    except ZeroDivisionError:
+        print("You can't divide a number by zero!")
 
+    print(f"The total amount inputted by the user is {total}" )
+    print(f"The average number is {average}")
+
+else:
+    concat = ""
+    for word in nums:
+        concat += word
+
+    print(concat)
 
